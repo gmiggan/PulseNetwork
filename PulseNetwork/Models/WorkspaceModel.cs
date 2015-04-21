14c5b225babfc94a6d00d6a0d02f090268d9bc3e
Gearoid Miggan <gmiggan2@hotmail.com>
2015-04-21 21:46:58 +0100

    Workspace added

---
+++ PulseNetwork/Models/WorkspaceModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace PulseNetwork.Models
+{
+    public class Workspace
+    {
+        [Key]
+        public int id { get; set; }
+        public String name { get; set; }
+        public String creatorID { get; set; }
+        [ForeignKey("creatorID")]
+        public virtual ApplicationUser creator { get; set; }
+    
+    }
+}
